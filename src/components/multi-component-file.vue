a3a4247bf22209ecb569d65345e34289c383b90e
Mike Ludemann <mike-ludemann@hotmail.de>
2020-04-22 18:26:28 +0200

    Setup complete - vue-multi-components-single-file

---
+++ src/components/multi-component-file.vue
@@ -0,0 +1,42 @@
+<script>
+
+export const ModernTable = {
+	props: {
+		id: {
+			type: String,
+			default: "test-table"
+		}
+	},
+	render(){
+		return (
+			<div>
+				<table id={this.id}>
+					{this.$slots.default}
+				</table>
+			</div>
+		)
+	}
+};
+
+export const ModernRow = {
+	props: {
+
+	},
+	render(){
+		return (
+			<tr>
+				{this.$slots.default}
+			</tr>
+		)
+	}
+};
+
+export default ModernTable
+
+</script>
+
+<style scoped lang="scss">
+	.multi-component-file {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<ModernTable id="hello">
+			<ModernRow>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+			</ModernRow>
+		</ModernTable>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ModernTable, ModernRow } from '@/components/multi-component-file.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		HelloWorld,
+		ModernTable,
+		ModernRow
+	}
+}
+</script>
